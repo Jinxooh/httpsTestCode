a6d887ebd9681db2c2180b552eff4ad95fa106fb
황진수-PC <cjswp122@gmail.com>
2018-02-13 13:22:10 +0900

    app js 로 다 옴김

--- src/app.js
+++ src/app.js
@@ -1,5 +1,28 @@
-const Server = require('./server');
+// const Server = require('./server');
 
-const server = new Server();
+// const server = new Server();
 
-server.httpsListen(8443);
+// server.httpsListen(8443);
+
+const express = require('express');
+ 
+const bodyParser = require('body-parser');
+
+const https = require('https');
+const fs = require('fs');
+
+const routes = require('./routes');
+
+const app = express();
+app.use(bodyParser.json());
+app.use('/', routes);
+
+const { app } = this;
+const rootDir = '/etc/letsencrypt/live/jadoochat.standard.kr';
+const options = {
+  ca: fs.readFileSync(`${rootDir}/chain.pem`),
+  key: fs.readFileSync(`${rootDir}/privkey.pem`),
+  cert: fs.readFileSync(`${rootDir}/cert.pem`),
+};
+
+https.createServer(options, app).listen(port, () => console.log('Https server listening on port ', port));
