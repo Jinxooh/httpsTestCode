b4599511a51cd6a34a950453073a5e5daab44fc0
황진수-PC <cjswp122@gmail.com>
2018-02-19 15:46:22 +0900

    http redirect made by myself

--- src/app.js
+++ src/app.js
@@ -27,11 +27,7 @@ app.get('/', function(request, response, next) {
 http_redirect.all('*', function(req, res, next) {
   if (/^http$/.test(req.protocol)) {
     var host = req.headers.host.replace(/:[0-9]+$/g, ""); // strip the port # if any
-    // if ((HTTPS_PORT != null) && HTTPS_PORT !== 443) {
-    //   return res.redirect("https://" + host + ":" + HTTPS_PORT + req.url, 301);
-    // } else {
     return res.redirect("https://" + host + req.url, 301);
-    // }
   } else {
     return next();
   }
